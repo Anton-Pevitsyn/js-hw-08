712741691f1f2207901cb1bcbc5febce072af769
Anton-Pevitsyn <75798691+Anton-Pevitsyn@users.noreply.github.com>
2022-04-09 17:33:43 +0400

    end

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import _ from "lodash";
+const iframe = document.querySelector("#vimeo-player");
+const player = new Vimeo.Player(iframe);
+const currentTimePlay = JSON.parse(
+  localStorage.getItem("videoplayer-current-time")
+);
+const throttleOnPlay = _.throttle(onPlay, 10);
 
+function onPlay(data) {
+  localStorage.setItem(
+    "videoplayer-current-time",
+    JSON.stringify(data.seconds)
+  );
+}
+
+player.on("timeupdate", throttleOnPlay);
+
+player
+  .setCurrentTime(currentTimePlay)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case "RangeError":
+        break;
+      default:
+        break;
+    }
+  });
+
+player
+  .getEnded()
+  .then(function (ended) {
+    localStorage.removeItem("videoplayer-current-time");
+  })
+  .catch(function (error) {});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import _ from "lodash";
+const refs = {
+  form: document.querySelector(".feedback-form"),
+  email: document.querySelector(".feedback-form input"),
+  message: document.querySelector(".feedback-form textarea"),
+};
+onInput();
 
+const formData = {};
+
+function submitForm(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem("feedback-form-state");
+}
+
+function setLocalStorage(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+refs.form.addEventListener("submit", submitForm);
+refs.form.addEventListener("input", _.throttle(setLocalStorage, 250));
+
+function onInput() {
+  const getLocalStarag = localStorage.getItem("feedback-form-state");
+  const parseObjects = JSON.parse(getLocalStarag);
+  console.log(getLocalStarag);
+  if (getLocalStarag) {
+    refs.email.value = parseObjects.email;
+    refs.message.value = parseObjects.message;
+  }
+}
